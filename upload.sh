a94df95991c81816dd428436a4ed56cf124358f3
keepraw <49091395+keepraw@users.noreply.github.com>
2025-04-23 14:01:02 +0800

    Update upload.sh (#296)

--- upload.sh
+++ upload.sh
@@ -2,8 +2,8 @@
 kill -9 $(ps aux | grep 'src.burn.scan' | grep -v grep | awk '{print $2}')
 kill -9 $(ps aux | grep '[u]pload' | awk '{print $2}')
 # start new process
-nohup python -m src.burn.scan > ./logs/runtime/scan-$(date +%Y%m%d-%H%M%S).log 2>&1 &
-nohup python -m src.upload.upload > ./logs/runtime/upload-$(date +%Y%m%d-%H%M%S).log 2>&1 &
+nohup python3 -m src.burn.scan > ./logs/runtime/scan-$(date +%Y%m%d-%H%M%S).log 2>&1 &
+nohup python3 -m src.upload.upload > ./logs/runtime/upload-$(date +%Y%m%d-%H%M%S).log 2>&1 &
 # Check if the last command was successful
 if [ $? -eq 0 ]; then
     echo "Success! Please ignore the 'kill: usage....' if it displays"
